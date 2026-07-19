5772780f9daa15b90a9e6e9182e624f2a7767a94
Shubham Raj <shubh@gmail.com>
2026-07-19 23:47:02 +0530

    Add 2-D array codes

---
+++ TwoDimnArray/DiagonalSumMatrix.java
@@ -0,0 +1,30 @@
+public class DiagonalSumMatrix {
+
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            // Primary diagonal
+            sum += matrix[i][i];
+
+            // Secondary diagonal
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - i - 1];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        int result = diagonalSum(matrix);
+        System.out.println("Diagonal Sum = " + result);
+    }
+}
---
+++ TwoDimnArray/matrices.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class matrices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int matrix[][] = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        // Input
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Output
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // new line after each row
+        }
+
+        sc.close();
+    }
+}
